6d387eb14d27cc9e83e02cb10995047d7f92cf8f
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-04-20 20:42:08 +0300

    create_search

--- src/index.js
+++ src/index.js
@@ -1,33 +1,62 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list')
-inputEl.addEventListener('input', onCountrySearch);
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+inputEl.addEventListener('input', debounce(onCountrySearch, DEBOUNCE_DELAY));
 
 function onCountrySearch() {
-  fetchCountries(inputEl.value)
+  fetchCountries(inputEl.value.trim())
     .then(data => {
       if (data.length > 10) {
-        console.log('too many matches'); // сделать красиво
-      } else {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length > 1) {
+        countryInfo.innerHTML = createPreviewMarkUp(data);
+        countryList.innerHTML = '';
+      } else if (data.length === 1) {
         countryList.innerHTML = createMarkUp(data);
+        countryInfo.innerHTML = '';
+      } else {
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
       }
     })
-    .catch(error => console.log(error));
+    .catch(error =>
+      Notiflix.Notify.failure('Oops, there is no country with that name.')
+    );
 }
 
+function createMarkUp(countriesArr) {
+  return countriesArr
+    .map(
+      ({
+        name: { official },
+        capital,
+        population,
+        flags: { svg },
+        languages,
+      }) => `<li class="country"><div class="flex">
+    <img width ="50" src="${svg}" alt="${official}">
+     <h2>${official}</h2></div>
+     <h3>Capital: ${capital} </h3>
+     <p>Population: ${population}</p>
+     <p>Languages: ${Object.values(languages)}</p>
+   </li>`
+    )
+    .join('');
+}
 
-function createMarkUp (countriesArr) {
-    return countriesArr.map(({name:{official}, capital,
-        population, flags:{svg}, languages}) => `<li>
-    <img src="${svg}" alt="${official}">
-     <h2>${official}</h2>
-     <h3>Capital:${capital} </h3>
-     <p>Population:${population}</p>
-     <p>Languages:${Object.values(languages)}</p>
-   </li>`).join('');
-   
-};
-
+function createPreviewMarkUp(countriesArr) {
+  return countriesArr
+    .map(
+      ({ name: { official }, flags: { svg } }) => `<li class="flex">
+      <img width ="50" src="${svg}" alt="${official}"> <p class="preview-name">${official}</p></li>`
+    )
+    .join('');
+}
